345fe95bc6839e3ec6a3abf7ac6410fa3f7b5f95
pachoremayuresh <pachoremayuresh@gmail.com>
2025-04-11 13:15:15 +0530

    Flying drone Version 1 with trail file to fly drone

--- Example_2_Existing_URDF_model/main.py
+++
@@ -1,9 +0,0 @@
-from mujoco import mjcf
-
-model = mjcf.from_path("Example_2_Existing_URDF_model/fiberthex/fiberthex.urdf")
-
-mjcf_str = model.to_xml_string()
-
-with open("convert_model.xml", "w") as f:
-    f.write(mjcf_str)
-    pass
